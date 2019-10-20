00cb011420357c2e0ef752049b195249c66f208e
charlieroth <charlieroth4@icloud.com>
2019-10-20 18:00:24 -0400

    Added tmux.conf in list of files to symlink in install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 # Variables
 DIR=~/dotfiles
 OLD_DIR=~/dotfiles_old
-FILES="gitconfig vimrc zshrc"
+FILES="gitconfig vimrc zshrc tmux.conf"
 
 # Create dotfiles_old in home directory
 echo "Creating $OLD_DIR for backup of any existing dotfiles in ~"
