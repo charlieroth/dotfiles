b27b863a98877383a6a9187b81b741d60606e6c8
Charles Roth <charlieroth4@icloud.com>
2021-03-14 08:35:55 -0400

    will split up nvim later

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 mkdir -p ~/.config/nvim/plugin
 mkdir -p ~/.config/nvim/after/plugin
 mkdir -p ~/.config/nvm/lua
@@ -6,8 +6,3 @@ mkdir -p ~/.config/nvm/lua
 ln -s $DOT/gitconfig $HOME/.gitconfig
 ln -s $DOT/zshrc $HOME/.zshrc
 ln -s $DOT/init.vim $HOME/.config/nvim/init.vim
-
-for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
-  rm -rf ~/.config/nvim/$f
-  ln -s ~/dotfiles/$f ~/.config/nvim/$f
-done
