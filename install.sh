d0389554d6a0684b3491579e314fc7031e3978f2
Charles Roth <charlieroth4@icloud.com>
2021-03-29 20:25:23 -0400

    Truly just dotfiles now

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-
-ln -s ~/dotfiles/zshrc ~/.zshrc
-
-mkdir -p ~/.config/nvim
-
-cp -Rv ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/.config/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.config/dotfiles/zshrc ~/.zshrc
