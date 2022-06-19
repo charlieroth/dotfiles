d0f4cb3e7680e73bae938bbd013f878156cf316b
Charles Roth <charlieroth4@icloud.com>
2022-06-19 20:59:32 +0200

    add tmux.conf

--- bash_profile
+++ bash_profile
@@ -1,5 +1,8 @@
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 if [ -n "$BASH_VERSION" ] && [ -f $HOME/.bashrc ];then
     source $HOME/.bashrc
 fi
+
--- setup.sh
+++ setup.sh
@@ -5,11 +5,13 @@ PROFILE="$HOME/github.com/charlieroth/dotfiles/profile"
 BASH_PROFILE="$HOME/github.com/charlieroth/dotfiles/bash_profile"
 VIM="$HOME/github.com/charlieroth/dotfiles/vimrc"
 GITCONFIG="$HOME/github.com/charlieroth/dotfiles/gitconfig"
+TMUX="$HOME/github.com/charlieroth/dotfiles/tmux.conf"
 
 ln -sf $PROFILE ~/.profile
 ln -sf $BASHRC ~/.bashrc
 ln -sf $BASH_PROFILE ~/.bash_profile
 ln -sf $GITCONFIG ~/.gitconfig
 ln -sf $VIM ~/.vimrc
+ln -sf $TMUX ~/.tmux.conf
 
 echo "dotfiles setup.sh: symlink complete"
