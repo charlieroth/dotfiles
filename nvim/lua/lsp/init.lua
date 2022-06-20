b10dea922641976e0af0157469c42d8d4346515d
Charles Roth <charlieroth4@icloud.com>
2022-06-20 21:28:18 +0200

    alias vim

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -2,8 +2,8 @@ local lspconfig = require('lspconfig')
 
 local servers = {
     { name = 'sumneko_lua', location = 'lsp.sumneko_lua', enabled = true },
-    { name = 'tsserver', location = 'lsp.tsserver', enabled = false },
-    { name = 'gopls', location = 'lsp.gopls', enabled = false },
+    { name = 'tsserver', location = 'lsp.tsserver', enabled = true },
+    { name = 'gopls', location = 'lsp.gopls', enabled = true },
     { name = 'zls', location = 'lsp.zls', enabled = false },
     { name = 'efm', location = 'lsp.efm', enabled = false },
 }
